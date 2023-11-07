0b3672eaff98d534e7193394c43aa5c37c86c164
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-11-07 18:08:21 +0500

    completed router user app with crud operations

--- express/controllers/user-controllers.js
+++ express/controllers/user-controllers.js
@@ -1,21 +1,83 @@
+let users = [
+  { name: "Yasir Ali", id: 100 },
+  { name: "Hussain", id: 101 },
+  { name: "Abrar Ali", id: 102 },
+];
+let id = 1;
+
 const getAllUsers = (req, res) => {
-  res.json("all users");
+  res.status(200).json({ users: users });
 };
 
 const createUser = (req, res) => {
-  res.json("creating user");
+  if (!req.body.name) {
+    res.status(401).json({ message: "please provide user name" });
+  }
+  const user = {
+    name: req.body.name,
+    id: id++,
+  };
+  users.push(user);
+  console.log(users);
+  res.status(201).json({ message: `user created successfully ${user}` });
 };
 
 const getUser = (req, res) => {
-  res.json(`getting specific user ${req.params.id}`);
+  const id = req.params.id.slice(1);
+
+  const specificUser = users.find((user) => {
+    if (user.id === Number(id)) {
+      return user;
+    }
+  });
+  if (!specificUser) {
+    res
+      .status(404)
+      .json({ success: false, message: "bad request user not found" });
+  }
+  res.status(200).json({ success: true, user: specificUser });
 };
 
 const deleteUser = (req, res) => {
-  res.json(`deleting specific user ${req.params.id}`);
+  const id = req.params.id.slice(1);
+  const updatedUsers = users.filter((user) => user.id !== Number(id));
+  const deletedUser = users.filter((user) => user.id === Number(id));
+  if (deletedUser.length < 1) {
+    res.status(404).json({ success: false, message: "user not found" });
+  }
+  users = [...updatedUsers];
+  res.status(200).json({ success: true, users: users });
 };
 
 const updateUser = (req, res) => {
-  res.json(`update user ${req.params.id}`);
+  const id = req.params.id.slice(1);
+  const specificUser = users.find((user) => user.id === Number(id));
+
+  if (!req.body.name) {
+    res
+      .status(401)
+      .json({ success: false, message: "please provide updated value" });
+  }
+
+  if (!specificUser) {
+    res
+      .status(404)
+      .json({ success: false, message: "user not found to update" });
+  }
+
+  const newUsers = users.map((user) => {
+    if (user.id === Number(id)) {
+      const obj = {
+        ...user,
+        name: req.body.name,
+      };
+      return obj;
+    }
+    return user;
+  });
+
+  users = [...newUsers];
+  res.json({ success: true, users: users });
 };
 
 module.exports = {
