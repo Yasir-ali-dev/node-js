43a0b02408e151b886dfeda68f3ce30bf9245ca6
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-11-13 23:56:40 +0500

    update and delete functionality added with mysql database

--- node-mysql/app.js
+++ node-mysql/app.js
@@ -36,6 +36,60 @@ app.get("/table", (req, res) => {
   });
 });
 
+const addRows = (id, name, email) => {
+  const insertQuery = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
+  const query = mysql.format(insertQuery, [
+    "employee",
+    "id",
+    "name",
+    "email",
+    id,
+    name,
+    email,
+  ]);
+  pool.query(query, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+};
+// addRows(1, "Sajid Ali", "sajid@gmail.com");
+
+const queryRow = (tableName, username, value) => {
+  // -- method-1 --
+  //   const insertQuery = "SELECT * FROM ?? WHERE ??=?";
+  //   const query = mysql.format(insertQuery, [tableName, username, value]);
+  const query = `SELECT * FROM ${tableName} WHERE ${username}="${value}"`;
+  pool.query(query, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+};
+// queryRow("employee", "name", "Sajid Ali");
+
+const updateRow = (
+  tableName,
+  updateFieldName,
+  updateFieldNameValue,
+  conditionField,
+  conditionFieldValue
+) => {
+  const query = `UPDATE ${tableName} SET ${updateFieldName}="${updateFieldNameValue}" WHERE ${conditionField}="${conditionFieldValue}"`;
+
+  pool.query(query, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+};
+// updateRow("employee", "id", 2, "name", "Sajid Ali");
+
+const deleteRow = (tableName, conditionName, conditionNameValue) => {
+  const query = `DELETE FROM ${tableName} WHERE ${conditionName}=${conditionNameValue}`;
+  pool.query(query, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+};
+
 app.listen(3001, () => {
   console.log("app is listening to the port 3001");
 });
