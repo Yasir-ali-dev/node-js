0336f093ae9b5ba8be0444ccfd0661e1d2c0704b
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-11-18 15:13:06 +0500

    implemented express-session basic system

--- auth-js/app.js
+++ auth-js/app.js
@@ -3,16 +3,26 @@ const session = require("express-session");
 
 const app = express();
 
-app.use(session({ secret: "my secret " }));
+app.use(
+  session({
+    secret: "my secret ",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.json());
 
+/*
 app.get("/", (req, res) => {
   req.session.username = "js";
-  res.json({ msg: "session is created" });
+  res.json({ msg: "session is created", session: `${req.session.username}` });
 });
 app.get("/get-session", (req, res) => {
   res.json({ msg: `your session's username ${req.session.username}` });
 });
+*/
 
+/*
 app.get("/count", (req, res) => {
   if (req.session.sessionId) {
     req.session.sessionId++;
@@ -21,6 +31,26 @@ app.get("/count", (req, res) => {
     req.session.sessionId = 1;
     res.json({ msg: "user visited first time" });
   }
+});
+*/
+
+app.get("/", (req, res) => {
+  if (req.session.authorized) {
+    res.send(`<h1>${req.session.user}</h1>`);
+  } else {
+    res.send(" login first");
+  }
+});
+
+app.get("/signout", (req, res) => {
+  req.session.destroy();
+  res.send(`<h1>logout successful</h1><h1>please login</h1>`);
+});
+
+app.get("/login", (req, res) => {
+  req.session.user = "yasir";
+  req.session.authorized = true;
+  res.send(`<h1>login successfully ${req.session.user}</h1>`);
 });
 
 app.listen(4000, () => {
