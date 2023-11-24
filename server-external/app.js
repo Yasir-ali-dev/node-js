ea1e801a6e33ce3e50a2c1aa796ae88f0adba9a5
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-11-24 23:49:51 +0500

    updated

--- server-external/app.js
+++ server-external/app.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const session = require("cookie-session");
+const connecDB = require("./db/connectDB");
+const errorHandler = require("./middlewares/errorHandler");
+const { default: mongoose } = require("mongoose");
+
+/*
 const redisClient = require("redis").createClient({
   host: "127.0.0.1",
   port: 6379,
 });
 app.set("view engine", "pug");
+
 // notice here we are telling express to render views using the pug templating engine.
 const colors = ["red", "green", "blue"];
 
@@ -34,6 +44,47 @@ app.get("/colors", (request, response) => {
   return response.render("data", { colors });
 });
 
-app.listen(4000, () => {
-  console.log("The server has started on port 4000");
-});
+*/
+app.use(session({ secret: "Do Not Reveal The Password" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new localStrategy(
+    // the first parameter is an optional object with options
+    {
+      // when using the local strategy you MUST name your keys usernameField and passwordField.
+      usernameField: "username",
+      passwordField: "secret",
+      passReqToCallback: true,
+      // by default this option is set to false, but when specified to true, the first parameter of the verify callback will be the request object. This is quite useful if you want to see if your application has multiple strategies and you want to see if a user is already logged in with an existing strategy,
+    },
+    function verifyCallback(req, username, Password, done) {}
+  )
+);
+console.log("here");
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+app.use(errorHandler);
+const port = process.env.PORT;
+const start = async () => {
+  try {
+    await connecDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`app is listening to the port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  mongoose.connection.on("error", (err) => {
+    console.log(err);
+  });
+};
+
+start();
