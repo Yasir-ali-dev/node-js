8cef3a1a40419f5c60e051c4053d152e99775e3b
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-12-09 14:13:16 +0500

    applied mutation with react apollo in qraphql

--- graph-ql/client/src/component/DisplayData.jsx
+++ graph-ql/client/src/component/DisplayData.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {useQuery, gql ,useLazyQuery  } from "@apollo/client";
+import {useQuery, gql ,useLazyQuery ,useMutation, } from "@apollo/client";
 
 const GET_ALL_USERS = gql`
   query getAllUsers {
@@ -8,11 +8,9 @@ const GET_ALL_USERS = gql`
       name
       nationality
       age
+      username
     }
   }
-
-  
-
 `;
 const GET_ALL_MOVIES = gql`
   query getAllMovies {
@@ -35,30 +33,152 @@ const GET_MOVIE_BY_NAME=gql`
   }
 `;
 
+const CREATE_USER =gql`
+  mutation createUser($input: CreateUserInput!){
+    createUser(input: $input) {
+      name
+      nationality
+      age
+      username
+    }
+  }
+`;
+
+const UPDATE_USERNAME =gql`
+  mutation updateUser($input: UpdateUsernameInput!){
+    updateUsername(input: $input) {
+      id
+      username
+    }
+  }
+`;
 
 const DisplayData = () => {
-    const {data , loading, error}=   useQuery(GET_ALL_USERS);
+    const {data , loading, error, refetch }= useQuery(GET_ALL_USERS);
     const {data : movies }= useQuery(GET_ALL_MOVIES);
     const [selectedQuery,setSelectedQuery]=useState("");
-    const [fetchData,{data: movieData, error: movieError}]=useLazyQuery(GET_MOVIE_BY_NAME);
+    const [fetchData,{data: movieData, error: movieError}]=
+    useLazyQuery(GET_MOVIE_BY_NAME);
+    const [createUser, setCreateUser]=useState({
+      name:"",
+      age: Number,
+      nationality:"",
+      username :"" 
+    });
+    const [userCreated] = useMutation(CREATE_USER);
+    
+    const [updateUsername,setUpdateUsername]=useState({id:Number, newUsername:""})
+    const [NewUsername] = useMutation(UPDATE_USERNAME);
 
-
-  //   if(data){
-  //       console.log(data);
-  //   }
-  //   if(movies){
-  //     console.log(movies);
-  // }
   if(movieData){
     console.log(movieData);
   }
 
+  const handleChange=(e)=>{
+    const {name, value}=e.target;
+    if(name === "nationality"){
+      setCreateUser((prevState)=>{
+        return{
+          ...prevState,
+          [name]: value.toUpperCase()
+        }
+      })  
+    }
+    else{
+      setCreateUser((prevState)=>{
+        return{
+          ...prevState,
+          [name]: value
+        }
+      })
+    }
+  }
 
-  
+  const handleChangeForUpdateUser=(e)=>{
+    const {name, value}=e.target;
+      setUpdateUsername((prevState)=>{
+        return{
+          ...prevState,
+          [name]: value
+        }
+      })  
     
+  }
+
   return (
     <>
     
+     <div>
+        <h1>Create New User</h1>
+        <input 
+          type="text" 
+          placeholder='Name ..' 
+          name='name' 
+          onChange={handleChange}
+          required
+        />
+        <input 
+          type="text" 
+          placeholder='Username ..' 
+          name='username' 
+          onChange={handleChange}
+          required
+        />
+        <input 
+          type="number" 
+          placeholder='Age ..'  
+          name='age'  
+          onChange={handleChange}
+          required
+        />
+        <input 
+          type="text" 
+          placeholder='Nationality..' 
+          name='nationality'  
+          onChange={handleChange} 
+          required
+        />
+        <button onClick={()=>{
+          userCreated({variables:
+            {
+              input:
+              {
+                name: createUser.name , 
+                username: createUser.username,
+                age: Number(createUser.age),
+                nationality: createUser.nationality
+              }
+              
+            }})
+            refetch()
+        } }>Submit</button>
+      </div> 
+
+
+      <div>
+        <h1>Update User</h1>
+        <input 
+          type="number" 
+          placeholder='Id ..' 
+          name='id' 
+          onChange={handleChangeForUpdateUser}
+          required
+        />
+        <input 
+          type="text" 
+          placeholder='New Username ..' 
+          name='newUsername' 
+          onChange={handleChangeForUpdateUser}
+          required
+        />
+        <button onClick={
+              ()=> {
+                NewUsername({variables:
+                {input:{id: Number(updateUsername.id), newUsername: updateUsername.newUsername}}})
+                refetch()
+              }
+            }>Update Username</button>
+      </div>
     <div style={{display:"flex",}}>
         {data &&
             data.users.map((_,index)=>{
@@ -71,6 +191,7 @@ const DisplayData = () => {
                         <h1>{_.name}</h1>
                         <h1>{_.age}</h1>
                         <h1>{_.nationality}</h1>
+                        <h1>{_.username}</h1>
                     </div>
                 )
             })
