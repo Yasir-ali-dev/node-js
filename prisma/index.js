adb49be24a3d2f454d4f39b0814f17039e38033a
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-12-15 00:39:49 +0500

    applied relation in mysql database in prisma

--- prisma/index.js
+++ prisma/index.js
@@ -6,7 +6,9 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const allUsers = await prisma.user.findMany();
+  const allUsers = await prisma.user.findMany({
+    include: { houseBuilt: true, houseOwned: true },
+  });
   res.json(allUsers);
 });
 
@@ -16,7 +18,7 @@ app.post("/", async (req, res) => {
 });
 
 app.put("/:id", async (req, res) => {
-  const id = parseInt(req.params.id.slice(1));
+  const id = req.params.id.slice(1);
   const newUser = await prisma.user.update({
     where: { id: id },
     data: req.body,
@@ -25,11 +27,23 @@ app.put("/:id", async (req, res) => {
 });
 
 app.delete("/:id", async (req, res) => {
-  const id = parseInt(req.params.id.slice(1));
+  const id = req.params.id.slice(1);
   const newUser = await prisma.user.delete({
     where: { id: id },
   });
   res.json(newUser);
+});
+
+app.post("/house", async (req, res) => {
+  const newHouse = await prisma.house.create({ data: req.body });
+  res.json(newHouse);
+});
+
+app.get("/house", async (req, res) => {
+  const houses = await prisma.house.findMany({
+    include: { owner: true, builtBy: true },
+  });
+  res.json(houses);
 });
 
 app.listen(4000, () => {
