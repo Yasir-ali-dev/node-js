36055c24fc325c6c04862e1a3a55649760228861
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-12-15 12:49:42 +0500

    completed simple project with prisma.io

--- prisma/index.js
+++ prisma/index.js
@@ -39,11 +39,54 @@ app.post("/house", async (req, res) => {
   res.json(newHouse);
 });
 
-app.get("/house", async (req, res) => {
+app.get("/houses", async (req, res) => {
   const houses = await prisma.house.findMany({
     include: { owner: true, builtBy: true },
   });
   res.json(houses);
+});
+
+app.get("/house/:id", async (req, res) => {
+  const id = req.params.id.slice(1);
+  const house = await prisma.house.findUnique({
+    where: { id: id },
+    include: { owner: true, builtBy: true },
+  });
+  res.json(house);
+});
+
+app.get("/house", async (req, res) => {
+  const { address } = req.body;
+  const house = await prisma.house.findUnique({
+    where: { address },
+  });
+  res.json(house);
+});
+
+app.get("/houses/filter", async (req, res) => {
+  const housesWithFilters = await prisma.house.findMany({
+    where: {
+      wifiPassword: { not: null },
+      owner: {
+        age: {
+          gte: 10,
+        },
+      },
+      // order by for sorting
+    },
+    orderBy: [
+      {
+        builtBy: {
+          firstName: "desc",
+        },
+      },
+    ],
+    include: {
+      owner: true,
+      builtBy: true,
+    },
+  });
+  res.json(housesWithFilters);
 });
 
 app.listen(4000, () => {
